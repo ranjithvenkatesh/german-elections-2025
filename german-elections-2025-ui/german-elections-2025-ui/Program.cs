b01c2f7e86bd6f48e37d45513002057911aa9c9e
Ranjith Venkatesh <Ranjith.Venkatesh@mossandlichens.com>
2025-02-01 17:23:24 +0100

    WebAssembly

---
+++ german-elections-2025-ui/german-elections-2025-ui/Program.cs
@@ -0,0 +1,33 @@
+using german_elections_2025_ui.Client.Pages;
+using german_elections_2025_ui.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(german_elections_2025_ui.Client._Imports).Assembly);
+
+app.Run();
